c006f463044a58af9328f78eec45cdb6e5e175a9
Raphael Carvalho <Rcfrdeveloper@gmail.com>
2024-09-23 12:30:58 -0400

    +1

---
+++ Sequencial/celsius.py
@@ -0,0 +1,7 @@
+f = float(input(
+    "entre com a temperatura em fahrenheit que deseja converter pra celsius: "))
+
+c = 5 * ((f - 32) / 9)
+
+print(f"a temperatura insirida convertida em celsius é aproximadamente {
+      c:.0f} graus")
