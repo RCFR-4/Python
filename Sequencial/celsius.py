ad0a6377ceb4e8b4aa7dbe8ebcccd207dfe10c3a
Raphael Carvalho <Rcfrdeveloper@gmail.com>
2024-09-23 12:34:47 -0400

    adicionando o enunciado esquecido

--- Sequencial/celsius.py
+++ Sequencial/celsius.py
@@ -1,3 +1,7 @@
+'''Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+    C = 5 * ((F-32) / 9)'''
+
+
 f = float(input(
     "entre com a temperatura em fahrenheit que deseja converter pra celsius: "))
 
