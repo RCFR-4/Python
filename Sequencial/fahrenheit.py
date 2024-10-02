b78b4332599cf90922a7ad50d6ba8ff3457ea04b
Raphael Carvalho <Rcfrdeveloper@gmail.com>
2024-10-02 11:06:33 -0400

    ultima algoritmo estrutura sequencial

---
+++ Sequencial/fahrenheit.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.#
+
+c = float(input(
+    "entre com a temperatura em celsius que deseja converter para fahrenheit: "))
+
+f = (c * 1.8) + 32
+
+print(f"o resultado da conversão é: {f} fahrenheit")
